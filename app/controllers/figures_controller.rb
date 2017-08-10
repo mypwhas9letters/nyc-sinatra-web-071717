1de75ec59de7a08870dc3d1b1bce821e708e7358
Hui Wang <huiw3939@gmail.com>
2017-08-10 17:49:40 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,35 @@
 class FiguresController < ApplicationController
 
+  get '/' do
+    erb:'/index'
+  end
+
   get '/figures/new' do
     erb:'/figures/new'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params['id'])
+    erb:"/figures/edit"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params['id'])
+    @figure.landmarks << Landmark.find_or_create_by(name: params['new_landmark'])
+    @figure.name = params['figure_name']
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+
+  end
+
+  # post '/' do
+  #   Figure.find_by(params['name'])
+  #   erb:""
+  # end
+
+  get '/figures/:id' do
+    @fig = Figure.find(params['id'])
+    erb:"/figures/show"
   end
 
   get '/figures' do
@@ -9,8 +37,22 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    Figure.create(params)
-    erb:'figures/index'
+    @fig = Figure.new(params['figure'])
+    # @fig.name = params['figure']['name']
+    # @fig.save
+
+    @title = Title.new(params['title'])
+    @fig.titles << @title
+    @title.save
+
+    @landmark = Landmark.new(params['landmark'])
+    @fig.landmarks << @landmark
+    @landmark.save
+
+
+    @fig.save
+
+    erb:'/figures/index'
   end
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,39 @@
 class LandmarksController < ApplicationController
 
+
+
+  get '/landmarks/new' do
+    erb:'/landmarks/new'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.all.find(params[:id])
+    erb:"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params['id'])
+    @landmark.name = params['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    erb:"landmarks/show"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.new(params['landmark'])
+    @landmark.name = params['landmark']['name']
+    @landmark.save
+    erb:'landmarks/index'
+  end
+
+  get '/landmarks' do
+    erb:'/landmarks/index'
+  end
+
+
+
 end
